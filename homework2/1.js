fc54d77d8bfae78c6205483545771f3e592f7db1
Ivan Ustyantsev <vangold915@gmail.com>
2023-11-03 16:45:17 +0700

    Task 1 completed

--- homework2/1.js
+++ homework2/1.js
@@ -18,3 +18,68 @@
 библиотеке и возвращать true или false в зависимости от того, есть ли такая 
 книга в списке или нет.
 */
+
+class Library {
+    #books = [];
+    constructor(books) {
+        Library.validateBookList(books);
+        this.#books = books;
+    }
+    static validateBookList(books) {
+        const booksSet = new Set();
+        books.forEach(el => {
+            booksSet.add(el);
+        });
+        if (booksSet.size !== books.length) {
+            throw new Error('Представленный массив содержит дубликаты!');
+        }
+    }
+    static bookExistValidation(bookList, book) {
+        if(bookList.indexOf(book) !== -1) {
+            throw new Error('Такая книга есть в списке!');
+        }
+    }
+    static bookNotExistValidation(bookList, book) {
+        if(bookList.indexOf(book) === -1) {
+            throw new Error('Такой книги нет в списке!');
+        }
+    }
+    get allBooks() {
+        return this.#books;
+    }
+    addBook(title) {
+        Library.bookExistValidation(this.#books, title);
+        this.#books.push(title);
+    }
+    removeBook(title) {
+        Library.bookNotExistValidation(this.#books, title);
+        this.#books = this.#books.filter(el => el !== title);
+    }
+    hasBook(title) {
+        return this.#books.indexOf(title) !== -1 ? true : false;
+    }
+}
+
+const books1 = ['Книга 1', 'Книга 1', 'Книга 1', 'Книга 1', 'Книга 1'];
+const books2 = ['Книга 1', 'Книга 2', 'Книга 3', 'Книга 4', 'Книга 5'];
+
+const library2 = new Library(books2);
+// const library1 = new Library(books1);
+
+library2.addBook('Книга 0');
+// library2.addBook('Книга 1');
+console.log(library2.allBooks);
+
+
+library2.removeBook('Книга 0');
+console.log(library2.allBooks);
+// library2.removeBook('Книга 0');
+// console.log(library2.allBooks);
+
+console.log(library2.hasBook('Книга 0'));
+console.log(library2.hasBook('Книга 1'));
+console.log(library2.hasBook('Книга 2'));
+console.log(library2.hasBook('Книга 3'));
+console.log(library2.hasBook('Книга 4'));
+console.log(library2.hasBook('Книга 5'));
+console.log(library2.hasBook('Книга 6'));
